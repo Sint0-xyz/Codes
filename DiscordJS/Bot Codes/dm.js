440228ea4b3e55e87d57cc637e37fd4305583665
Sint0 <101415806+Sint0-xyz@users.noreply.github.com>
2022-03-15 18:50:46 +0100

    Create dm.js

---
+++ DiscordJS/Bot Codes/dm.js
@@ -0,0 +1,9 @@
+module.exports = (client, triggerText, replyText) => {
+    client.on('message', message => {
+        if (message.content.toLowerCase() === triggerText.toLowerCase()) {
+            message.author.send(replyText)
+        }
+    })
+}
+
+// **Only dm** = message.channel.type === 'dm' && 
